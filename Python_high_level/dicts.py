34af2ece4cada3536b5ed2933c73396f84d3289d
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-14 15:08:53 -0400

    un diccionartio tine un una clacve y valor,usar del para elimar cualquiercosa

--- Python_high_level/dicts.py
+++ Python_high_level/dicts.py
@@ -7,4 +7,7 @@ print(type(mi_otro_dict))
 mi_otro_dict = {'nombre':'bryan','apellido':'camacho','edad':35}
 print(mi_otro_dict)
 
+del mi_dic
+#usar del para elimar cualquiercosa
+
 #un diccionartio tine un una clacve y valor
