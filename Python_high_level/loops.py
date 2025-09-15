5ffe64c886ebec4b2eef154469510fc9bbe1e7b6
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-15 17:30:48 -0400

    while loops with else condition

---
+++ Python_high_level/loops.py
@@ -0,0 +1,17 @@
+#loops
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+else:
+    print('lo demas de el bucle')  
+
+
+
+
+#for ()
+
+
+
+
+#loops es para pasar el codigo varias veces iterar
