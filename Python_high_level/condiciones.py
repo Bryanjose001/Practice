091ce343825946837fd945abfebe95a7e9fa63ea
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-15 15:11:06 -0400

    tratando las condiciones de if else si se cumplen

--- Python_high_level/condiciones.py
+++ Python_high_level/condiciones.py
@@ -5,3 +5,9 @@ if my_condition:
     print('si es es true aparace3 el if')
 
 print('la condicion continua')
+
+segunda_cond = 2 + 5
+
+if segunda_cond >= 7:
+    print('es mayor que 7')
+else: ('no es mayor que 7')
