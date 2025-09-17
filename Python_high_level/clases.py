fbb2b81056346ca49cc897c9d5d628dd34e772dd
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-17 01:38:41 -0400

    ya ahora funciona la clase y printea la persona

--- Python_high_level/clases.py
+++ Python_high_level/clases.py
@@ -2,7 +2,9 @@
 
 class Persona:
     def __init__(self,name,surname,age):
-        pass
+        self.name = name
+        self.surname = surname
+        self.age = age
     
 my_persona = Persona('Bryan','Camacho',23)
-print(my_persona)
+print(my_persona.name,my_persona.surname,my_persona.age)
