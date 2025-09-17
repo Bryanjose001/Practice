00b40119be7720f0e201b4df05493c112bdfaf27
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-17 00:47:39 -0400

    tarttando nuevas function

--- Python_high_level/function.py
+++ Python_high_level/function.py
@@ -9,3 +9,11 @@ def sum_two_values(first_number, second_number):
     print(first_number+second_number)
 
 sum_two_values(5,2)
+
+#def sum_two_values(first_number, second_number):
+    #print(first_number+second_number)
+    
+    #return(5,2)
+
+
+
