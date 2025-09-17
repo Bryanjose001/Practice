535d9555fd6dbdaf74e2c33f45f1b2507854384c
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-17 00:35:10 -0400

    creando uba  funcion de suma

--- Python_high_level/function.py
+++ Python_high_level/function.py
@@ -4,3 +4,8 @@ def my_function ():
     print('esto es un funcion')
 
 my_function()
+
+def sum_two_values(first_number, second_number):
+    print(first_number+second_number)
+
+sum_two_values(5,2)
