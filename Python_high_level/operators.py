7768e23e534bd7bdf881294c35ed83f64aaaa1ae
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-19 13:46:34 -0400

    operator practice 2

--- Python_high_level/operators.py
+++ Python_high_level/operators.py
@@ -5,4 +5,20 @@ print('CR7\n' * (my_int))
 
 age = int(23)
 height = float(5.10)
-var = complex()
+a = 7
+b = 6
+var = complex(a , b)
+
+base = float(input('enter base: '))
+height = int(input('enter height: '))
+area = 0.5 * base * height 
+print(area)
+
+
+sidea = int(input('the side a is: '))
+sideb = int(input('the side b is: '))
+sidec = int(input('the side c is: '))
+perimeter = sidea + sideb + sidec
+print (perimeter)
+
+
