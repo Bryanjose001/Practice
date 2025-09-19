ff8f9797d8862ddc9d978d7084a4978354c974d7
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-18 20:27:17 -0400

    variables

--- Python_high_level/varibles.py
+++ Python_high_level/varibles.py
@@ -1,4 +1,6 @@
-first_name = "Bryan"
+
+age = 23
+country = 'Puerto Rico'
 num1 = 7
 numero3 = 4.31
 is_true = True 
@@ -11,6 +13,14 @@ num_two = 4
 total = 9
 diff = num_one - num_two
 product = num_two * num_one
+r = 30
+area_of_circle = 3.14 * (r**2)
+circum_of_circle = 2 * 3.14 * r
+
+variable = input('mesaje para el usuario')
+first_name = input('tu Primer nombre: ')
+last_name = input('tu apellido: ')
+
 
 
 print(f'mi nombre es {name}, mi segundo es {surname} y mi edad es {age}')
@@ -20,3 +30,6 @@ print(len(name))
 print(len(surname))
 print(diff)
 print(product)
+print(area_of_circle)
+print(circum_of_circle)
+
