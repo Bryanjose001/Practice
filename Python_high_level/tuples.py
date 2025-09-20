2970537b46faf572b036b734eb070b5f85224245
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-20 15:59:37 -0400

    lists 05

--- Python_high_level/tuples.py
+++ Python_high_level/tuples.py
@@ -1,11 +1,24 @@
 #tuple
 my_tuple = tuple()
 my_other_tuple = ()
-
 my_tuple = (35,45,'hola')
+mis_hermanos = ('Alejandro','Joha','Bryan')
+mis_padres = ('Nilma','Alberto')
+mi_familia = mis_hermanos + mis_padres
+alejandro,joha, bryan,nilma,alberto = mi_familia
+frutas = ('manzana','pera','akakkak')
+vegetales = ('cebolla','tomate','lalal')
+productos_animales = ('comida','baalal','lol')
+food_stuff_tp = frutas + vegetales + productos_animales
+food_stuff_tp = list(food_stuff_tp)
+food_stuff_lt = food_stuff_tp
 
 
+
+
+print(len(food_stuff_lt))
 print(my_other_tuple)
 print(my_tuple)
+print(len(mis_hermanos))
 
 #tuple is the same as a list but it connt be mutablable
