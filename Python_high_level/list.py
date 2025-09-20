8fd6dbd247dbcf1b5d78dd9766b4b42ecfc43cfe
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-20 14:53:14 -0400

    lists01

--- Python_high_level/list.py
+++ Python_high_level/list.py
@@ -1,6 +1,8 @@
 # lstas 
-my_list = ()
-mi_otra_lista = ['bryan',27,7]
+my_list = list()
+mi_otra_lista = ['bryan',27,7,3,4]
 
 print(type(my_list))
 print(mi_otra_lista)
+print(len(mi_otra_lista))
+print[mi_otra_lista(0,2,4)]
