df656e41b7ce59467413636679567a25531013d1
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-22 04:44:41 -0400

    condiciones 00

--- Python_high_level/condiciones.py
+++ Python_high_level/condiciones.py
@@ -13,6 +13,23 @@ if segunda_cond:
 else: 
     print('no es mayor que 7')
 
+Edad = 18
+Edad = int(input('Cual es tu edad: '))
+if Edad>= 18:
+    print('you are old enough to learn to drive')
+else:
+    print('you need more years to drive')
+
+my_age = 23
+your_age = (input('cual es tu edad: ?'))
+if your_age <= 23:
+    print('estas cb')
+else:
+    print()
+
+
+
+
 
 
 #condiciones son instrucciones que permiten a un programa tomar decisiones y ejecutar diferentes bloques de código según si se cumple un requisito
