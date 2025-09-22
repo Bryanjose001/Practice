0abdbaec622fb2d861f379a38fb0569906f2286e
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-22 03:12:21 -0400

    dictionary 03

--- Python_high_level/dictionaries.py
+++ Python_high_level/dictionaries.py
@@ -3,6 +3,7 @@ mi_dic = dict ()
 mi_otro_dict = {}
 mi_otro_dict = {'nombre':'bryan','apellido':'camacho','edad':35}
 dog = {'name':'pepe','color':'blonde','breed':'golden retriever','age':23}
+student = {'first_name':'Bryan','last_name ':'Camacho','gender':'male','age':23}
 
 
 
@@ -12,6 +13,7 @@ del mi_dic
 print(mi_otro_dict)
 print(type(mi_otro_dict))
 print(dog)
+print(len(student))
 
 
 
