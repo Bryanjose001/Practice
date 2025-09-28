0693e4587fba9417af20d5236473b4df4c813db8
Bryanjose001 <bryan.jose2541@gmail.com>
2025-09-28 17:16:09 -0400

    loops practice 00

--- Python_high_level/loops.py
+++ Python_high_level/loops.py
@@ -1,15 +1,20 @@
 #loops
 my_condition = 0
 
-while my_condition < 10:
+'''while my_condition < 10:
     print(my_condition)
     my_condition += 2
 else:
-    print('mi condicion es mayor o igual que 10')  
+    print('mi condicion es mayor o igual que 10''' 
 
-my_list = [1,2,3,4]
+'''my_list = [1,2,3,4]
 for element in my_list:
-    print(element)
+    print(element)'''
+
+
+while my_condition < 11:
+    print(my_condition)
+    my_condition += 1
 
 
 
