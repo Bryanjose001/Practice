534d92f26fb394cbb1b8348543ba7624d1f108ba
Bryanjose001 <bryan.jose2541@gmail.com>
2025-10-02 15:11:58 -0400

    function practice

--- Python_high_level/function.py
+++ Python_high_level/function.py
@@ -1,6 +1,6 @@
 #function
 
-def my_function ():
+'''def my_function ():
     print('esto es un funcion')
 
 my_function()
@@ -10,10 +10,17 @@ def sum_two_values(first_number, second_number):
 
 sum_two_values(5,2)
 
-#def sum_two_values(first_number, second_number):
-    #print(first_number+second_number)
+def sum_two_values(first_number, second_number):
+    print(first_number+second_number)
     
-    #return(5,2)
+    #return(5,2)'''
+
+
+
+
+
+
+
 
 
 
