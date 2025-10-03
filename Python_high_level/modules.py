8dd66a751171668582d546efc5a9e995ca35ae70
Bryanjose001 <bryan.jose2541@gmail.com>
2025-10-03 17:47:43 -0400

    modules practice 02

--- Python_high_level/modules.py
+++ Python_high_level/modules.py
@@ -2,9 +2,14 @@
 '''from function import sum_two_values
 
 function.sum_two_values(3,7)'''
-from random import random 
+from random import random
+import string 
 
 def random_number_id(): 
-    random_user_id = 54321
-    return random_number_id
+    random_user_id = ''
+    characters = string.ascii_letters + string.digits
+    for y in range(6):
+       
+       pass 
+    return random_user_id
 print(random_number_id())
