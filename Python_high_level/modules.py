720c33fdcbe8625f73d0d90002604b985035c068
Bryanjose001 <bryan.jose2541@gmail.com>
2025-10-03 18:08:05 -0400

    modules practice 03

--- Python_high_level/modules.py
+++ Python_high_level/modules.py
@@ -18,16 +18,24 @@ print(random_number_id())
 
 def user_id_gen_by_user(): 
     random_user_id = ''
+    num_char = int(input('how many char in id: '))
+    num_ids = int(input('how many id you want: '))
     characters = string.ascii_letters + string.digits
     for y in range(6):
         random_user_id += random.choice(characters)
-        num_char = input
-        num_ids = input
         list_of_id =[]
-        num_char + num_ids = list_of_id
+        num_char + num_ids == list_of_id
 
-    for elment in 
-    return random_user_id
+    for elment in range(num_ids):
+        current_id = ''
+        random_user_id += random.choice(characters)
+
+        for elment in range(num_char):
+            random_char += current_id
+            random_user_id += random.choice(characters)
+            list_of_id.append(current_id)
+
+    return list_of_id
 
 print(user_id_gen_by_user())
 
