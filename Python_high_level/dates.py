d48e1362f1aac369cba613327b760573eaae6322
Bryanjose001 <bryan.jose2541@gmail.com>
2025-10-06 17:44:46 -0400

    dates

--- Python_high_level/dates.py
+++ Python_high_level/dates.py
@@ -1,4 +1,20 @@
 #dates
 from datetime import datetime
+now =  datetime.now()
 
-print(datetime.hour)
+print(now.year)
+print(now.month)
+print(now.day)
+print(now.hour)
+print(now.minute)
+print(now.second)
+print(now.microsecond)
+
+
+timestamp = now.timestamp()
+
+print(timestamp)
+
+
+year_2026 = datetime(2026,1,1)
+
