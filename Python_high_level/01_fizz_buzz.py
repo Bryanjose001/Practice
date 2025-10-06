bd781f974766ad8d9cf96b8549089c99d41f1d94
Bryanjose001 <bryan.jose2541@gmail.com>
2025-10-06 16:37:43 -0400

    fizz buzz desde 0

--- Python_high_level/01_fizz_buzz.py
+++ Python_high_level/01_fizz_buzz.py
@@ -13,19 +13,3 @@ Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
 
 
 def fizz_buzz():
-    for number in range(1,101):
-        if number % 5 == 0 and number % 3 == 0:
-            number = 'fizzbuzz'
-        elif number % 3 == 0:
-            number = 'fizz'
-        elif number % 5 == 0:
-            number = 'buzz' 
-        print(number,'\n')
-fizz_buzz()
-
-
-
-
-
-
-
