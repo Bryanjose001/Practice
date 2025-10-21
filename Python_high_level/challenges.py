6efae5f38cfe12deeeabaecc4c2ba0429de74f96
Bryanjose001 <bryan.jose2541@gmail.com>
2025-10-21 12:46:07 -0400

    challenges

--- Python_high_level/challenges.py
+++ Python_high_level/challenges.py
@@ -20,9 +20,16 @@ while my_buzz < 100:
         print('buzz')
     else:
         print(my_buzz)
-'''
+    '''
 
 def fizzbuzz():
-    for index in range(1,100):
-        print(index)
+    for index in range(1,101):
+        if index % 3 == 0 and index % 5 == 0:
+            print('fizzbuzz')
+        elif index % 3 == 0:
+            print('fizz')
+        elif index % 5 == 0:
+            print('buzz')
+        else:
+            print(index)
 fizzbuzz()
