93cf0219aaafcefba0ea695c1786ec099e877b4a
Bryanjose001 <bryan.jose2541@gmail.com>
2025-10-21 12:26:52 -0400

    fizz buzz 2

--- Python_high_level/challenges.py
+++ Python_high_level/challenges.py
@@ -9,4 +9,15 @@ Múltiplos de 5 por la palabra "buzz".
 Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  '''
 
+my_buzz = 0
+while my_buzz < 100:
+    my_buzz = my_buzz + 1
+    if my_buzz % 3 == 0 and my_buzz % 5 == 0:
+        print('fizz buzz')
+    elif my_buzz % 3 == 0:
+        print('fizz')
+    elif my_buzz % 5 == 0:
+        print('buzz')
+    else:
+        print(my_buzz)
 
