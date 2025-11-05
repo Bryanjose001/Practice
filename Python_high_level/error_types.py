cd64bd00a52f221f1b0bf37348faee3c23ba8414
Bryanjose001 <bryan.jose2541@gmail.com>
2025-11-05 17:10:38 -0400

    Error types

--- Python_high_level/error_types.py
+++ Python_high_level/error_types.py
@@ -2,9 +2,9 @@
 
 # SyntaxError
 # print "¡Hola comunidad!" # Descomentar para Error
-print'hola mundo'
 print('hola mundo')
 
 # NameError
 language = "Spanish"  # Comentar para Error
 print(language)
+
