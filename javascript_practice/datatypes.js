3c02be72f9276fa91cad03928d12938c887c3a18
Bryanjose001 <bryan.jose2541@gmail.com>
2025-11-09 14:12:58 -0400

    javascript learning

---
+++ javascript_practice/datatypes.js
@@ -0,0 +1,16 @@
+// tipos de datos primitivos
+let name = 'Bryan Jose'
+
+//numeros (numbers)
+let age =23
+
+//booleans
+let isStudent=true
+let isTeacher=false
+
+//undefined
+let undefinedvalue
+console.log(undefinedvalue)
+
+//Null
+let nullvalue = null
