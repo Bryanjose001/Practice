8ac643af55c3f503a31f7458333e69720b2f24e5
Bryanjose001 <bryan.jose2541@gmail.com>
2025-11-12 16:25:43 -0400

    javascript strings

---
+++ javascript_practice/strings-exercises.js
@@ -0,0 +1,45 @@
+// 1. Concatena dos cadenas de texto
+let name = 'Bryan'
+let greeting = 'Hey, '+ name + '!'
+console.log(greeting)
+
+
+// 2. Muestra la longitud de una cadena de texto
+console.log(greeting.length)
+
+
+
+// 3. Muestra el primer y último carácter de un string
+console.log(greeting[0])
+console.log(greeting[11])
+
+
+
+// 4. Convierte a mayúsculas y minúsculas un string
+console.log(greeting.toUpperCase()) 
+console.log(greeting.toLowerCase())
+
+
+
+
+// 5. Crea una cadena de texto en varias líneas
+let varias_lineas = `voubwvbuojwqbvowbvbuowqb
+bgbhvgqwbbv ob
+geujoieqbvg
+ujbbedvbwqo`
+
+
+
+// 6. Interpola el valor de una variable en un string
+
+
+
+
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+// 9. Comprueba si dos strings son iguales
+
+// 10. Comprueba si dos strings tienen la misma longitud
